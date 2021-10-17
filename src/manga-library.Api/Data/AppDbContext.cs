3661978b30fc2129257c41cc2ababa7ab44d1a3c
Leonardo Lima Cavalcante <leonardo.cavalcante@mottu.com.br>
2021-10-17 18:38:12 -0300

    add efcore migrations configuration

---
+++ src/manga-library.Api/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using manga_library.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace manga_library.Api.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Readers> Readers { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder.UseSqlite("DataSource=app.db;Cache=Shared");
+    }
+}
--- src/manga-library.Api/Startup.cs
+++ src/manga-library.Api/Startup.cs
@@ -33,7 +33,6 @@ namespace manga_library.Api
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Manga Library API", Version = "v1" });
             });
-            services.AddAutoMapper(typeof(AutoMapperSetup.AutoMapperSetup));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
